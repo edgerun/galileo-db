d1c086ff303a38a12acb50303f6c98855677abf7
Thomas Rausch <thomas@rauschig.org>
2020-10-11 23:43:55 +0200

    Release version 0.5.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="galileo-db",
-    version="0.4.0",
+    version="0.5.0",
     author="Thomas Rausch",
     author_email="t.rausch@dsg.tuwien.ac.at",
     description="Galileo DB: Gateway and client tools for the Galileo Experiment DB",
