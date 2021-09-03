e2d34247aa65f76c91e6cd32b14317ff83428c54
Philipp <poepsi39@gmail.com>
2021-09-03 18:38:49 +0200

    Refactor test_db.py

--- tests/influx/test_db.py
+++ tests/influx/test_db.py
@@ -1,19 +1,18 @@
 import time
 import unittest
 
-
 from galileodb.factory import create_influxdb_from_env
 from galileodb.model import ExperimentEvent, RequestTrace
 
 
 class TestInfluxExperimentDatabase(unittest.TestCase):
 
-
     def setUp(self) -> None:
         self.exp_db = create_influxdb_from_env()
         self.exp_db.open()
         self.exp_id = 'exp-1'
-        self.exp_bucket = self.exp_db.client.buckets_api().create_bucket(bucket_name=self.exp_id, org_id='a0ec9985b4a69119')
+        self.exp_bucket = self.exp_db.client.buckets_api().create_bucket(bucket_name=self.exp_id,
+                                                                         org_id='a0ec9985b4a69119')
 
     def tearDown(self) -> None:
         self.exp_db = create_influxdb_from_env()
@@ -39,8 +38,7 @@ class TestInfluxExperimentDatabase(unittest.TestCase):
 
     def test_save_traces(self):
         exp_id = self.exp_id
-        exp_db = create_influxdb_from_env()
-        exp_db.open()
+        exp_db = self.exp_db
 
         original_traces = [
             RequestTrace('1', 'client-1', 'resnet', time.time(), time.time() + 0.01, time.time() + 0.1, 200, 'server-1',
@@ -48,8 +46,6 @@ class TestInfluxExperimentDatabase(unittest.TestCase):
         ]
         exp_db.save_traces(original_traces)
 
-        traces = exp_db.get_traces('exp-1')
+        traces = exp_db.get_traces(exp_id)
 
         self.assertEquals(original_traces, traces, 'traces are not equal')
-
-        exp_db.close()
