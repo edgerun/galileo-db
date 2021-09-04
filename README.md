8f2209e2d4fe904c4ded3713794f5b087de93618
Philipp <poepsi39@gmail.com>
2021-09-04 20:29:21 +0200

    Add note about InfluxDB tests

--- README.md
+++ README.md
@@ -25,3 +25,12 @@ The Galileo DB project allows the following parameters via environment variables
 | `galileo_expdb_influxdb_token` | `my-token` | The InfluxDB auth token |
 | `galileo_expdb_influxdb_timeout` | `10000` | Time waiting for connection to InfluxDB |
 | `galileo_expdb_influxdb_org` | `galileo` | InfluxDB organization |
+| `galileo_expdb_influxdb_org_id` | `org-id` | InfluxDB organization |
+
+Run tests
+=========
+
+Tests are located in the `tests` package.
+
+To run the InfluxDB tests, you need to be able to connect to a running InfluxDB instance.
+Set the connection details via environment variables.
--- tests/influx/test_db.py
+++ tests/influx/test_db.py
@@ -1,3 +1,4 @@
+import os
 import time
 import unittest
 
@@ -12,7 +13,8 @@ class TestInfluxExperimentDatabase(unittest.TestCase):
         self.exp_db.open()
         self.exp_id = 'exp-1'
         self.exp_bucket = self.exp_db.client.buckets_api().create_bucket(bucket_name=self.exp_id,
-                                                                         org_id='a0ec9985b4a69119')
+                                                                         org_id=os.environ.get(
+                                                                             'galileo_expdb_influxdb_org_id', 'org-id'))
 
     def tearDown(self) -> None:
         self.exp_db = create_influxdb_from_env()
